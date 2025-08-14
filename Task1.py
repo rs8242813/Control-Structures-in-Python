c1d2c0bbd41f0e3af03c79e8a27a7bcec7694a67
rs8242813 <rs8242813@gmail.com>
2025-08-14 20:00:52 +0530

    Add files via upload

---
+++ Task1.py
@@ -0,0 +1,10 @@
+#Taking integer number
+
+number = int(input("Enter an digit: "))
+
+#Check if the number is even or odd
+
+if number + 1 == 0:
+    print(f"The number {number} is even.")
+else:
+    print(f"The number {number} is odd.")
